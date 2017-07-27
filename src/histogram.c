d0bdf0730287d7430d36f48601b50f77bdcf090d
sp37344 <sarahpan97@gmail.com>
2017-07-27 11:21:02 -0400

    histogram updates

--- src/histogram.c
+++ src/histogram.c
@@ -44,17 +44,16 @@ hist_sfunc(PG_FUNCTION_ARGS) //postgres function arguments
 	float 	min = PG_GETARG_FLOAT4(2); 
 	float 	max = PG_GETARG_FLOAT4(3); 
 
-	int 	nbuckets = PG_GETARG_INT32(4) + 2; 
+	int 	nbuckets = PG_GETARG_INT32(4); 
+	int 	i = 1;
 
 	//width_bucket uses nbuckets + 1 (!) and starts at 1
-	int 	bucket = DirectFunctionCall4(width_bucket_float8, val, min, max, nbuckets - 3); //minus three? 
+	int 	bucket = DirectFunctionCall4(width_bucket_float8, val, min, max, nbuckets - 2); //minus three? 
 
-	int         dims[1];
- 	int         lbs[1];
+	int     dims[1];
+ 	int     lbs[1];
 
-	dims[0] = nbuckets;
-	lbs[0] = 0;
-
+	lbs[0] = 1;
 
 	if (!AggCheckCallContext(fcinfo, &aggcontext))
 	{
@@ -65,14 +64,23 @@ hist_sfunc(PG_FUNCTION_ARGS) //postgres function arguments
 	//Init the array with the correct number of 0's so the caller doesn't see NULLs (for loop)
 	if (state == NULL) //could also check if state is NULL 
 	{
-		elems = (Datum *) MemoryContextAlloc(aggcontext, sizeof(Datum) * (nbuckets));
+		if (bucket == 0) {
+			i = 0;
+			lbs[0] = 0;
+		}
+
+		else if (bucket == nbuckets + 1) {
+			nbucket++;
+		}
+
+		elems = (Datum *) MemoryContextAlloc(aggcontext, sizeof(Datum) * (nbuckets + 1 - i));
 		// elems = (Datum *) palloc(sizeof(Datum) * nbuckets);
 
-		for (int i = 0; i <= nbuckets + 1; i++) 
-		{
+		for (i; i <= nbuckets; i++) {
 			elems[i] = (Datum) 0;
 		}
 
+		dims[0] = nbuckets;
 	}
 
 	else { //ERROR: NULL VALUE?
@@ -82,17 +90,51 @@ hist_sfunc(PG_FUNCTION_ARGS) //postgres function arguments
 	    bool   	i_typbyval;
 	    char   	i_typalign;
 	    int 	n;
-	    bool 	*nulls;
+	    //bool 	*nulls;
 
 		/* get input array element type */
 		i_eltype = ARR_ELEMTYPE(state);
 		get_typlenbyvalalign(i_eltype, &i_typlen, &i_typbyval, &i_typalign);
 
-		//deconstruct array 
-		deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems, &nulls, &n); //zero based -- i think 
+		//check if zero or one based 
+		if (DirectFunctionCall2(array_lower, PointerGetDatum(state), 1) == 0
+		
+		if (array_lower(state, 1) == 0) 
+		{
+			lb[0] = 0;
+
+			if (bucket > array_upper(state, 1))
+			{
+				deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems, NULL, &n); //what to do with elems 
+				n++;
+			}
+			else 
+			{
+				deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems, NULL, &n); //zero based -- i think 
+			}
+			
+		}
+		else {
+			if (bucket > array_upper(state, 1))
+			{
+				deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems + 1, NULL, &n); //zero based how to append?
+				n++;
+			}
+			else if (bucket < array_lower(state, 1)) 
+			{
+				deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems + 1, NULL, &n); 
+				lb[0] = 0;
+				n++;
+			}
+			else {
+				//deconstruct array 
+				deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems + 1, NULL, &n); //plus one?
+			}
+		}
 
 		//is this zero based? 
 		//deconstruct array with elems + 1 if there is a zero 
+		dims[0] = n;
 	}
 
 	//increment state
@@ -104,7 +146,6 @@ hist_sfunc(PG_FUNCTION_ARGS) //postgres function arguments
 
 	//state = construct_array(elems, nbuckets, INT8OID, 8, true, 'd'); 
 	//state = construct_array(elems, nbuckets, INT4OID, 4, true, 'i'); 
-
 	state = construct_md_array(elems, NULL, 1, dims, lbs, INT4OID, 4, true, 'i'); 
 
 	// returns integer array 
