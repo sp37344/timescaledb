a308e7d83405afa08824b8f378831fb259a83450
sp37344 <sarahpan97@gmail.com>
2017-07-28 11:59:47 -0400

    histogram updates

--- test/sql/histogram_test.sql
+++ test/sql/histogram_test.sql
@@ -1,6 +1,6 @@
 \ir include/create_single_db.sql
 
--- Test 1
+-- table 1
 CREATE TABLE "hitest1"(key real, val varchar(40));
 
 -- insertions
@@ -15,12 +15,21 @@ INSERT INTO "hitest1" VALUES(1, 'sup');
 INSERT INTO "hitest1" VALUES(2, 'hello');
 INSERT INTO "hitest1" VALUES(1, 'sup');
 
+-- table 2
+CREATE TABLE "hitest2"(name varchar(30), score integer, qualify boolean);
+
+-- insertions
+INSERT INTO "hitest2" VALUES('Tom', 6, TRUE);
+INSERT INTO "hitest2" VALUES('Mary', 4, FALSE);
+INSERT INTO "hitest2" VALUES('Jaq', 3, FALSE);
+INSERT INTO "hitest2" VALUES('Jane', 10, TRUE);
+
 -- standard 2 bucket
 SELECT histogram(key, 0, 10, 2) FROM hitest1;
 -- standard multi-bucket 
 SELECT histogram(key, 0, 10, 5) FROM hitest1;
 -- standard extra bucket 
-SELECT histogram(key, 0, 7, 3) FROM hitest1 GROUP BY val;
+SELECT val, histogram(key, 0, 7, 3) FROM hitest1 GROUP BY val;
 -- standard element beneath lb
 SELECT histogram(key, 1, 7, 3) FROM hitest1;
 -- standard element above ub
@@ -32,15 +41,6 @@ SELECT histogram(key, 1, 3, 2) FROM hitest1;
 SELECT histogram(key, 1, 3, 1) FROM hitest1;
 -- error testing: max > min
 SELECT histogram(key, 5, 3, 2) FROM hitest1;
-
--- Test 2
-CREATE TABLE "hitest2"(name varchar(30), score integer, qualify boolean);
-
--- insertions
-INSERT INTO "hitest2" VALUES('Tom', 6, TRUE);
-INSERT INTO "hitest2" VALUES('Mary', 4, FALSE);
-INSERT INTO "hitest2" VALUES('Jaq', 3, FALSE);
-INSERT INTO "hitest2" VALUES('Jane', 10, TRUE);
 
 -- standard 2 bucket 
 SELECT qualify, histogram(score, 0, 10, 2) FROM hitest2 GROUP BY qualify;
