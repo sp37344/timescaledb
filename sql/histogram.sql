b9af24345f6a52d152064c987c6af5e8a932ca35
sp37344 <sarahpan97@gmail.com>
2017-08-02 11:38:20 -0400

    Add histogram-discrete

--- sql/histogram.sql
+++ sql/histogram.sql
@@ -3,6 +3,11 @@ RETURNS INTEGER[]
 AS '$libdir/timescaledb', 'hist_sfunc'
 LANGUAGE C IMMUTABLE;
 
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_sfunc (state INTEGER[], val REAL, MIN REAL, MAX REAL, nbuckets INTEGER) 
+RETURNS INTEGER[] 
+AS '$libdir/timescaledb', 'hist_sfunc'
+LANGUAGE C IMMUTABLE;
+
 CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_combinefunc(state1 INTEGER[], state2 INTEGER[])
 RETURNS INTEGER[]
 AS '$libdir/timescaledb', 'hist_combinefunc'
@@ -12,6 +17,15 @@ LANGUAGE C IMMUTABLE;
 DROP AGGREGATE IF EXISTS histogram (REAL, REAL, REAL, INTEGER);
 CREATE AGGREGATE histogram (REAL, REAL, REAL, INTEGER) (
        SFUNC = _timescaledb_internal.hist_sfunc,
+       STYPE = INTEGER[],
+       COMBINEFUNC = _timescaledb_internal.hist_combinefunc,
+       PARALLEL = SAFE
+);
+
+-- Tell Postgres how to use the new function
+DROP AGGREGATE IF EXISTS histogram (REAL, REAL[]);
+CREATE AGGREGATE histogram (REAL, REAL[]) (
+       SFUNC = _timescaledb_internal.hist_sfunc_discrete,
        STYPE = INTEGER[],
        COMBINEFUNC = _timescaledb_internal.hist_combinefunc,
        PARALLEL = SAFE
--- src/histogram.c
+++ src/histogram.c
@@ -7,9 +7,11 @@
 
 /* aggregate histogram:
  *	 hist(state, val, min, max, nbuckets) returns the histogram array with nbuckets
+ *	 hist(state, val, threshold[]) returns the histogram array 
  *
  * Usage:
  *	 SELECT hist(field, min, max, nbuckets) FROM table GROUP BY parameter.
+ *	 SELECT hist(field, threshold) FROM table GROUP BY parameter.
  */
 
 PG_FUNCTION_INFO_V1(hist_sfunc);
@@ -42,7 +44,6 @@ hist_sfunc(PG_FUNCTION_ARGS)
 		/* Cannot be called directly because of internal-type argument */
 		elog(ERROR, "hist_sfunc called in non-aggregate context");
 	}
-
 
 	if (min > max) {
 		elog(ERROR, "lower bound cannot exceed upper bound");
@@ -128,6 +129,104 @@ hist_sfunc(PG_FUNCTION_ARGS)
 			}
 			elems_edit[bucket] = (Datum) 0;
 			elems = elems_edit;
+		}
+
+		/* Set number of buckets */
+		dims[0] = n;
+	}
+
+	/* Increment the proper bucket */
+	elems[bucket-s] = elems[bucket-s] + (Datum) 1; 
+
+	/* Construct the state array */
+ 	state = construct_md_array(elems + lbs[0] - s, NULL, 1, dims, lbs, INT4OID, 4, true, 'i'); 
+
+	/* Return the integer array */
+	PG_RETURN_ARRAYTYPE_P(state); 
+}
+
+/* Generate a histogram */
+Datum
+hist_sfunc_discrete(PG_FUNCTION_ARGS) 
+{
+	MemoryContext aggcontext; 
+	ArrayType 	*state = PG_ARGISNULL(0) ? NULL : PG_GETARG_ARRAYTYPE_P(0);
+	Datum 		*elems; 
+
+	Datum 		val = PG_GETARG_DATUM(1);
+	// float 		val = PG_GETARG_FLOAT4(1); // DATUM? see postgres docs
+	ArrayType 	*thresholds = PG_GETARG_ARRAYTYPE_P(2);
+
+	int 	bucket = DirectFunctionCall2(width_bucket_array, val, thresholds); 
+	int 	nbuckets =  DirectFunctionCall2(array_upper, PointerGetDatum(thresholds), 1);
+
+	int     dims[1]; // 1-D array containing number of buckets used to construct histogram
+ 	int     lbs[1]; // 1-D array containing the lower bound used to construct histogram
+ 	int 	s = 0; // Extra indexing variable for conversion between C and PostgreSQL arrays
+
+ 	/* Determine the lower bound (i.e. zero- or one-basing in PostgreSQL array) */
+ 	lbs[0] = (bucket == 0) ? 0 : 1;
+
+	if (!AggCheckCallContext(fcinfo, &aggcontext))
+	{
+		/* Cannot be called directly because of internal-type argument */
+		elog(ERROR, "hist_sfunc called in non-aggregate context");
+	}
+
+	/* check if array is sorted  before directfunctionalcall4 */
+
+	/* Init the array with the correct number of 0's so the caller doesn't see NULLs (for loop) */
+	if (state == NULL)  
+	{
+		/* Construct a zero-based array representation in C (+1 accounts for the zero-th element) */
+		elems = (Datum *) MemoryContextAlloc(aggcontext, sizeof(Datum) * (nbuckets + 1)); 
+
+		for (int i = 0; i <= nbuckets; i++) {
+			elems[i] = (Datum) 0;
+		}
+
+		/* Specify the number of elements by accounting for buckets outside of the input range */
+		dims[0] = nbuckets + 1 - lbs[0];
+	}
+
+	else { 
+		Oid    	i_eltype;
+	    int16  	i_typlen;
+	    bool   	i_typbyval;
+	    char   	i_typalign;
+	    int 	n;
+	    // bool 	*nulls;
+	    /* Copy of elems if needed */
+	    Datum 	*elems_edit;
+
+		/* Get input array element type */
+		i_eltype = ARR_ELEMTYPE(state);
+		get_typlenbyvalalign(i_eltype, &i_typlen, &i_typbyval, &i_typalign);
+
+		/* Deconstruct array */
+		deconstruct_array(state, i_eltype, i_typlen, i_typbyval, i_typalign, &elems, NULL, &n); 
+
+		/* Specify zero-based array if the state array already contains a zero index */
+		if (DirectFunctionCall2(array_lower, PointerGetDatum(state), 1) == 0) {
+			lbs[0] = 0;
+		}
+
+		else if (bucket < DirectFunctionCall2(array_lower, PointerGetDatum(state), 1)) {
+			n++;
+
+			/* Copy array with an added zero index */
+			elems_edit = (Datum *) MemoryContextAlloc(aggcontext, sizeof(Datum) * n);
+			elems_edit[0] = (Datum) 0;
+
+			for (int j = 1; j <= n; j++) {
+				elems_edit[j] = elems[j - 1];
+			}
+			elems = elems_edit;
+		}
+
+		/* Otherwise specify one-based array in PostgreSQL */
+		else { 
+			s = 1;
 		}
 
 		/* Set number of buckets */
