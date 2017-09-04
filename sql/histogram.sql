93d659b8ac5aa10d7e1b00549c1b7be62179ed1f
sp37344 <sarahpan97@gmail.com>
2017-09-04 17:42:06 -0400

    histogram that takes in an array of thresholds

--- sql/histogram.sql
+++ sql/histogram.sql
@@ -3,22 +3,32 @@ RETURNS INTERNAL
 AS '$libdir/timescaledb', 'hist_sfunc'
 LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_combinefunc(state1 INTERNAL, state2 INTERNAL)
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_sfunc_discrete (state INTERNAL, val DOUBLE PRECISION, threshold REAL[]) 
+RETURNS INTERNAL 
+AS '$libdir/timescaledb', 'hist_sfunc_discrete'
+LANGUAGE C IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_combinefunc (state1 INTERNAL, state2 INTERNAL)
 RETURNS INTERNAL
 AS '$libdir/timescaledb', 'hist_combinefunc'
 LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_serializefunc(INTERNAL)
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_serializefunc (INTERNAL)
 RETURNS bytea
 AS '$libdir/timescaledb', 'hist_serializefunc'
 LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_deserializefunc(bytea, INTERNAL)
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_deserializefunc (bytea, INTERNAL)
 RETURNS INTERNAL
 AS '$libdir/timescaledb', 'hist_deserializefunc'
 LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_finalfunc(state INTERNAL, val DOUBLE PRECISION, MIN REAL, MAX REAL, nbuckets INTEGER) 
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_finalfunc (state INTERNAL, val DOUBLE PRECISION, MIN REAL, MAX REAL, nbuckets INTEGER) 
+RETURNS INTEGER[]
+AS '$libdir/timescaledb', 'hist_finalfunc'
+LANGUAGE C IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION _timescaledb_internal.hist_finalfunc_discrete (state INTERNAL, val DOUBLE PRECISION, threshold REAL[]) 
 RETURNS INTEGER[]
 AS '$libdir/timescaledb', 'hist_finalfunc'
 LANGUAGE C IMMUTABLE;
@@ -33,5 +43,18 @@ CREATE AGGREGATE histogram (DOUBLE PRECISION, REAL, REAL, INTEGER) (
     DESERIALFUNC = _timescaledb_internal.hist_deserializefunc,
     PARALLEL = SAFE,
     FINALFUNC = _timescaledb_internal.hist_finalfunc,
+    FINALFUNC_EXTRA
+);
+
+-- Tell Postgres how to use the new function
+DROP AGGREGATE IF EXISTS histogram (DOUBLE PRECISION, REAL[]);
+CREATE AGGREGATE histogram (DOUBLE PRECISION, REAL[]) (
+    SFUNC = _timescaledb_internal.hist_sfunc_discrete,
+    STYPE = INTERNAL,
+    COMBINEFUNC = _timescaledb_internal.hist_combinefunc,
+    SERIALFUNC = _timescaledb_internal.hist_serializefunc,
+    DESERIALFUNC = _timescaledb_internal.hist_deserializefunc,
+    PARALLEL = SAFE,
+    FINALFUNC = _timescaledb_internal.hist_finalfunc_discrete,
     FINALFUNC_EXTRA
 );
--- src/histogram.c
+++ src/histogram.c
@@ -22,10 +22,12 @@
  */
 
 PG_FUNCTION_INFO_V1(hist_sfunc);
+PG_FUNCTION_INFO_V1(hist_sfunc_discrete);
 PG_FUNCTION_INFO_V1(hist_combinefunc);
 PG_FUNCTION_INFO_V1(hist_serializefunc);
 PG_FUNCTION_INFO_V1(hist_deserializefunc);
 PG_FUNCTION_INFO_V1(hist_finalfunc);
+PG_FUNCTION_INFO_V1(hist_finalfunc_discrete);
 
 /* histogram(state, val, min, max, nbuckets) */
 Datum
@@ -58,6 +60,49 @@ hist_sfunc(PG_FUNCTION_ARGS)
 	{
 		/* Allocate memory to a new histogram state array */
 		Size		arrsize = sizeof(Datum) * (nbuckets + 2);
+
+		state = MemoryContextAllocZero(aggcontext, VARHDRSZ + arrsize);
+		SET_VARSIZE(state, VARHDRSZ + arrsize);
+	}
+
+	/* Increment the proper histogram bucket */
+	hist = (Datum *) VARDATA(state);
+	hist[bucket] = Int32GetDatum(DatumGetInt32(hist[bucket]) + 1);
+
+	PG_RETURN_BYTEA_P(state);
+}
+
+/* histogram(state, val, threshold) */
+Datum
+hist_sfunc(PG_FUNCTION_ARGS)
+{
+	MemoryContext aggcontext;
+	bytea	   *state = (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+	Datum	   *hist;
+
+	float		val = PG_GETARG_DATUM(1);
+	Datum 		threshold = PG_GETARG_DATUM(2);
+
+	int 		nbuckets =  DirectFunctionCall2(array_upper, thresholds, 1);
+	// int			bucket = DirectFunctionCall4(width_bucket_float8, val, min, max, nbuckets);
+	int 		bucket = DirectFunctionCall2(width_bucket_array, val, threshold); 
+
+	if (!AggCheckCallContext(fcinfo, &aggcontext))
+	{
+		/* cannot be called directly because of internal-type argument */
+		elog(ERROR, "hist_sfunc called in non-aggregate context");
+	}
+
+	if (min > max)
+	{
+		/* cannot generate a histogram with incompatible bounds */
+		elog(ERROR, "lower bound cannot exceed upper bound");
+	}
+
+	if (state == NULL)
+	{
+		/* Allocate memory to a new histogram state array */
+		Size		arrsize = sizeof(Datum) * (nbuckets + 2); /* CHANGE */
 
 		state = MemoryContextAllocZero(aggcontext, VARHDRSZ + arrsize);
 		SET_VARSIZE(state, VARHDRSZ + arrsize);
@@ -168,9 +213,36 @@ hist_deserializefunc(PG_FUNCTION_ARGS)
 	PG_RETURN_BYTEA_P(state);
 }
 
-/* hist_funalfunc(internal, val REAL, MIN REAL, MAX REAL, nbuckets INTEGER) => INTEGER[] */
+/* hist_funalfunc(internal, val DOUBLE PRECISION, MIN REAL, MAX REAL, nbuckets INTEGER) => INTEGER[] */
 Datum
 hist_finalfunc(PG_FUNCTION_ARGS)
+{
+	bytea	   *state;
+	Datum	   *hist;
+	int			dims[1];
+	int			lbs[1];
+
+	if (!AggCheckCallContext(fcinfo, NULL))
+	{
+		/* cannot be called directly because of internal-type argument */
+		elog(ERROR, "hist_finalfunc called in non-aggregate context");
+	}
+
+	if (PG_ARGISNULL(0))
+		PG_RETURN_NULL();
+
+	state = PG_GETARG_BYTEA_P(0);
+	hist = (Datum *) VARDATA(state);
+
+	dims[0] = HIST_LEN(state);
+	lbs[0] = 1;
+
+	PG_RETURN_ARRAYTYPE_P(construct_md_array(hist, NULL, 1, dims, lbs, INT4OID, 4, true, 'i'));
+}
+
+/* hist_funalfunc_discrete(internal, val DOUBLE PRECISION, threshold REAL[]) => INTEGER[] */
+Datum
+hist_finalfunc_discrete(PG_FUNCTION_ARGS)
 {
 	bytea	   *state;
 	Datum	   *hist;
